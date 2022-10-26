30b863c52671198613131be2a9f52d0c391a68f8
zemira kehati <kehati10@gmail.com>
2022-10-27 01:58:42 +0300

    set functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,36 @@
-import { Api } from "../components/Api.js";
+import { getPostsByUserId, getUsersInfo } from "../components/api.js";
+
 import { User } from "../components/User.js";
 import { Post } from "../components/Post.js";
 import { Section } from "../components/Section.js";
-
-const api = new Api({
-  baseUrl: "https://jsonplaceholder.typicode.com",
-});
 
 const users = new Section({ renderer: createUser }, ".users");
 const posts = new Section({ renderer: createPost }, ".posts-container");
 
 function createPost(data) {
-  const post = new Post(
-    {
-      data,
-    },
-    ".post-template",
-  );
+  const post = new Post({
+    data,
+  });
   posts.addItem(post.getPost());
 }
 
 function createUser(data) {
-  const user = new User(
-    {
-      data,
-      handleUserClick: () => {
-        Promise.all([api.getPostsByUserId(data.id)])
-          .then(([postData]) => {
-            posts.renderItems(postData);
-          })
-          .catch((err) => console.log(`Error: ${err}`));
-      },
+  const user = new User({
+    data,
+    handleUserClick: () => {
+      getPostsByUserId(data.id)
+        .then((postData) => {
+          posts.removeItems();
+          posts.renderItems(postData);
+        })
+        .catch((err) => console.log(`Error: ${err}`));
     },
-    ".user-template",
-  );
+  });
   users.addItem(user.getUser());
 }
 
-Promise.all([api.getUsersInfo()])
-  .then(([userData]) => {
+getUsersInfo()
+  .then((userData) => {
     users.renderItems(userData);
   })
   .catch((err) => console.log(`Error: ${err}`));
